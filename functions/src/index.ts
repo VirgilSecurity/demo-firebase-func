fa3d396d5286c26786e37efcfcfcead287ee3c5c
Alexey Smirnov <alexey@smirnov.works>
2018-11-05 18:48:15 +0200

    added support for push notification

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,38 +1,48 @@
-import * as functions from 'firebase-functions';
-import * as express from 'express';
-import * as admin from 'firebase-admin';
-import * as cors from 'cors';
-import { JwtGenerator } from 'virgil-sdk';
-import { VirgilCrypto, VirgilAccessTokenSigner } from 'virgil-crypto';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
+import * as cors from "cors";
+import { JwtGenerator } from "virgil-sdk";
+import { VirgilCrypto, VirgilAccessTokenSigner } from "virgil-crypto";
 
 const app = express();
 admin.initializeApp();
 
 interface IRequestWithFirebaseUser extends express.Request {
-    user: admin.auth.DecodedIdToken;
+  user: admin.auth.DecodedIdToken;
 }
 
-const validateFirebaseIdToken = (req: IRequestWithFirebaseUser, res: express.Response, next: express.NextFunction) => {
-  console.log('Check if request is authorized with Firebase ID token');
-  
-  if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer '))) {
-    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
-        'Make sure you authorize your request by providing the following HTTP header:',
-        'Authorization: Bearer <Firebase ID Token>');
-    res.status(403).send('Unauthorized');
+const validateFirebaseIdToken = (
+  req: IRequestWithFirebaseUser,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  console.log("Check if request is authorized with Firebase ID token");
+
+  if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) {
+    console.error(
+      "No Firebase ID token was passed as a Bearer token in the Authorization header.",
+      "Make sure you authorize your request by providing the following HTTP header:",
+      "Authorization: Bearer <Firebase ID Token>"
+    );
+    res.status(403).send("Unauthorized");
     return;
   }
 
-  const idToken = req.headers.authorization.split('Bearer ')[1];
-  console.log('id token', idToken)
-  admin.auth().verifyIdToken(idToken).then((decodedIdToken) => {
-    console.log('ID Token correctly decoded', decodedIdToken);
-    req.user = decodedIdToken;
-    next();
-  }).catch((error) => {
-    console.error('Error while verifying Firebase ID token:', error);
-    res.status(401).send('Unauthorized');
-  });
+  const idToken = req.headers.authorization.split("Bearer ")[1];
+  console.log("id token", idToken);
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedIdToken => {
+      console.log("ID Token correctly decoded", decodedIdToken);
+      req.user = decodedIdToken;
+      next();
+    })
+    .catch(error => {
+      console.error("Error while verifying Firebase ID token:", error);
+      res.status(401).send("Unauthorized");
+    });
 };
 
 const crypto = new VirgilCrypto();
@@ -44,10 +54,10 @@ const generator = new JwtGenerator({
   accessTokenSigner: new VirgilAccessTokenSigner(crypto)
 });
 
-app.use(cors({ origin: true, methods: 'OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE', }));
+app.use(cors({ origin: true, methods: "OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE" }));
 app.use(validateFirebaseIdToken);
-app.post('/generate_jwt', (req: IRequestWithFirebaseUser, res: express.Response) => {
-  const identity = req.user.email.replace('@virgilfirebase.com', '');
+app.post("/generate_jwt", (req: IRequestWithFirebaseUser, res: express.Response) => {
+  const identity = req.user.email.replace("@virgilfirebase.com", "");
   const virgilJwtToken = generator.generateToken(identity);
   res.json({ token: virgilJwtToken.toString() });
 });
@@ -56,3 +66,49 @@ app.post('/generate_jwt', (req: IRequestWithFirebaseUser, res: express.Response)
 // Requests need to be authorized by providing an `Authorization` HTTP header
 // with value `Bearer <Firebase ID Token>`.
 exports.api = functions.https.onRequest(app);
+
+exports.createUser = functions.firestore
+  .document("Channels/{channelId}/Messages/{messageId}")
+  .onCreate((snap, context) => {
+    console.log(snap.data(), context);
+    const message = snap.data();
+    const receiver = message.receiver;
+    console.log("receiver", receiver);
+    admin
+      .firestore()
+      .collection("Users")
+      .doc(receiver)
+      .get()
+      .then(doc => {
+        console.log("message", message);
+        const user = doc.data();
+        const notification = {
+          notification: {
+            title: "new message",
+            body: message.body
+          },
+          apns: {
+            payload: {
+              aps: {
+                "mutable-content": 1
+              }
+            }
+          },
+          token: user.registration_token
+        };
+        console.log(notification);
+
+        // Send a message to the device corresponding to the provided
+        // registration token.
+        admin
+          .messaging()
+          .send(notification)
+          .then(response => {
+            // Response is a message ID string.
+            console.log("Successfully sent message:", response);
+          })
+          .catch(error => {
+            console.log("Error sending message:", error);
+          });
+      });
+  });
